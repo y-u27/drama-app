cae5121faf1116333698856070e3030ede342cf5
Atd27 <y518a480@gmail.com>
2025-06-17 01:21:37 +0900

    投稿作成・削除処理追加

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,11 @@
 use App\Http\Controllers\DramaController;
 use Illuminate\Support\Facades\Route;
 
+// 投稿データ表示
 Route::get('/drama', [DramaController::class, 'index'])->name('drama.index1');
+
+// 投稿作成
+Route::post('/drama/create', [DramaController::class, 'create'])->name('drama.create');
+
+// 投稿削除
+Route::delete('/drama/{id}', [DramaController::class, 'destroy'])->name('drama.destroy');
