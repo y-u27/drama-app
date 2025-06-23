59b0dfcb4cb9ec1e8d3c1adb34979b9819aac888
Atd27 <y518a480@gmail.com>
2025-06-24 00:05:23 +0900

    投稿ページ表示ルート・投稿内容保存ルート追加

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,14 @@
 use App\Http\Controllers\DramaController;
 use Illuminate\Support\Facades\Route;
 
-// 投稿データ表示
-Route::get('/drama', [DramaController::class, 'index'])->name('drama.index1');
+// 投稿一覧表示
+Route::get('/drama', [DramaController::class, 'index'])->name('drama.index');
 
-// 投稿作成
-Route::post('/drama/create', [DramaController::class, 'create'])->name('drama.create');
+// 投稿ページ表示
+Route::get('/drama/create', [DramaController::class, 'create'])->name('drama.create');
+
+// 投稿内容保存
+Route::post('/drama', [DramaController::class, 'store'])->name('drama.store');
 
 // 投稿削除
 Route::delete('/drama/{id}', [DramaController::class, 'destroy'])->name('drama.destroy');
