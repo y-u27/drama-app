ae08d34b356d35135a13dcc2f67ea1d89cf727bb
Atd27 <y518a480@gmail.com>
2025-11-13 23:56:56 +0900

    メインページ作成開始

---
+++ resources/views/main.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    @vite('resources/css/app.css')
+    <title>メインページ</title>
+</head>
+<body class="bg-gradient-to-l from-indigo-300 via-blue-200 to-sky-200">
+    <div>ドラマの感想を書く！</div>
+    <div>ドラマカレンダー作成へ</div>
+</body>
+</html>
