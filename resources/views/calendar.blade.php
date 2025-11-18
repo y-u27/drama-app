b46aebd39ba977ac35de9477334e2a9587cf7acf
Atd27 <y518a480@gmail.com>
2025-11-18 23:55:24 +0900

    カレンダーページ作成

---
+++ resources/views/calendar.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ドラマカレンダー</title>
+</head>
+
+<body>
+  カレンダー機能実装予定
+  <a href="{{ route('main.index') }}">メインページへ戻る</a>
+</body>
+
+</html>
