009faa32cff0f7efa1a6be97ae5bb3a82cd3d27b
Atd27 <y518a480@gmail.com>
2025-11-19 23:59:31 +0900

    FullCalendar導入

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -4,11 +4,23 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+  @vite('resources/css/app.css')
   <title>ドラマカレンダー</title>
 </head>
+<script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.19/index.global.min.js'></script>
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth'
+    });
+    calendar.render();
+  });
+</script>
 
-<body>
-  カレンダー機能実装予定
+<body class="bg-gradient-to-l from-indigo-300 via-blue-200 to-sky-200">
+  <div id='calendar' class="w-300 h-150 bg-white p-10"></div>
   <a href="{{ route('main.index') }}">メインページへ戻る</a>
 </body>
 
