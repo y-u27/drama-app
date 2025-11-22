89cbba01d5ec5d047e27a7d8c1b7d26a1084f48d
Atd27 <y518a480@gmail.com>
2025-11-22 23:58:17 +0900

    ドラマカレンダー作成ページへのボタンUI修正

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -16,7 +16,7 @@
                 ドラマの感想を書く！
             </a>
         </div>
-        <div class="p-25 w-70 h-70 bg-white rounded">
+        <div class="p-25 w-70 h-70 bg-white rounded group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-transparent px-6 font-medium text-neutral-600 transition-all duration-100 [box-shadow:5px_5px_rgb(82_82_82)] active:translate-x-[3px] active:translate-y-[3px] active:[box-shadow:0px_0px_rgb(82_82_82)]">
             <a href="{{ route('calendar.index') }}">ドラマカレンダー作成へ</a>
         </div>
     </div>
