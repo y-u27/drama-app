fd407c965b060e1fb102728634c9ccfe2446de83
Atd27 <y518a480@gmail.com>
2026-03-07 23:51:28 +0900

    必須項目追記

--- app/Models/Calendar.php
+++ app/Models/Calendar.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Calendar extends Model
 {
     protected $fillable = [
-        'title'
+        'title',
+        'country',
+        'body',
     ];
 }
