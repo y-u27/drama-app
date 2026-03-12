f1aa404ee75ddbcbc2ceabc42aab8e0f870b1fdb
Atd27 <y518a480@gmail.com>
2026-03-12 23:57:38 +0900

    日付登録のためのコード追記中

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -19,18 +19,20 @@ class CalendarController extends Controller
         return view('calendarRegister');
     }
 
-    public function store(Request $request) 
+    public function store(Request $request)
     {
         $validate = $request->validate([
             'title' => 'required|string|max:255',
             'country' => 'required|string|max:255',
             'body' => 'nullable|string',
+            // 'update' => 'nullable|string'
         ]);
 
         Calendar::create([
             'title' => $validate['title'],
             'country' => $validate['country'],
             'body' => $validate['body'] ?? null,
+            // 'update' => $validate['update'] ?? null
         ]);
 
         return redirect('/calendar')->with('message', '登録しました');
