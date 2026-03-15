44e30938b186de1c90820e7481e29c701e381e65
Atd27 <y518a480@gmail.com>
2026-03-16 02:09:53 +0900

    登録したドラマのカレンダー表示のためのJSコード追記

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -8,25 +8,32 @@
   @vite('resources/css/app.css')
   <title>ドラマカレンダー</title>
 </head>
-<script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.19/index.global.min.js'></script>
-<script>
-  document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth'
-    });
-    calendar.render();
-  });
-</script>
 
 <body class="bg-gradient-to-l from-indigo-300 via-blue-200 to-sky-200">
   <div class="px-30 py-7">
-    <div id='calendar' class="w-300 h-150 p-10 border-3 border-blue-900 border-solid bg-white rounded"></div>
+    <div id='calendar' data-events='@json($events)' class="w-300 h-150 p-10 border-3 border-blue-900 border-solid bg-white rounded"></div>
   </div>
   <div class="px-10 pb-3 text-lg">
     <a href="{{ route('main.index') }}">メインページへ戻る</a>
-    <a href="{{ route('calenderRegister.create') }}">登録</a>
+    <a href="{{ route('calendarRegister.create') }}">登録</a>
   </div>
+
+  <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.19/index.global.min.js'></script>
+  <script>
+    document.addEventListener('DOMContentLoaded', function() {
+      var calendarEl = document.getElementById('calendar');
+
+      // PHP から渡された $events を JSON化
+      var eventsData = JSON.parse(calendarEl.getAttribute('data-events'));
+
+      var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        locale: 'ja',
+        events: eventsData
+      });
+      calendar.render();
+    });
+  </script>
 </body>
 
 </html>
