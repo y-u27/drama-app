3bfb5d18c4f6f1e53cc8cd99423970f2135ec98e
Atd27 <y518a480@gmail.com>
2026-03-16 02:08:36 +0900

    カレンダー表示とカレンダー上に登録内容を反映させるための機能追加

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -10,10 +10,12 @@ class CalendarController extends Controller
     public function index()
     {
         // カレンダー表示
-        return view('calendar');
+        $events = Calendar::all(['title', 'start', 'country', 'body']);
+
+        return view('calendar', compact('events'));
     }
 
-    public function calenderRegister()
+    public function calendarRegister()
     {
         // カレンダー登録フォーム表示
         return view('calendarRegister');
@@ -25,16 +27,16 @@ class CalendarController extends Controller
             'title' => 'required|string|max:255',
             'country' => 'required|string|max:255',
             'body' => 'nullable|string',
-            // 'update' => 'nullable|string'
+            'start' => 'required|date'
         ]);
 
         Calendar::create([
             'title' => $validate['title'],
             'country' => $validate['country'],
             'body' => $validate['body'] ?? null,
-            // 'update' => $validate['update'] ?? null
+            'start' => $validate['start'],
         ]);
 
-        return redirect('/calendar')->with('message', '登録しました');
+        return redirect()->route('calendar.index')->with('message', '登録しました');
     }
 }
